c89f09fbd8e4b02420a5839f75d6a292818b1ffa
RichardFFrank <71610664+RichardFFrank@users.noreply.github.com>
2022-03-08 00:03:28 -0500

    initial commit of parsetagram

--- parstagram/AppDelegate.swift
+++ parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        let parseConfig = ParseClientConfiguration{
+            $0.applicationId = "v9HZFdBvtjCW7VyNvyIN8wpyru0FLP3oYyuIIoj5"
+            $0.clientKey = "evtOJAl0SQl4K6RCSnDGV8XIE1CXjcNpzcbvDOPu"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        
         return true
     }
 
